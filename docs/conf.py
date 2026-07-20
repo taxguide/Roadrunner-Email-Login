27b4e3c127f2fe12b947db25c92e9b099e2a973e
taxguide <singhparash06@gmail.com>
2026-07-20 14:55:01 +0530

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -1,1 +1,27 @@
+# Configuration file for the Sphinx documentation builder.
 
+project = 'Roadrunner Email Login'
+author = 'Roadrunner Email Login'
+release = '1.0'
+
+extensions = [
+    'sphinx_sitemap',
+]
+
+# Templates
+templates_path = ['_templates']
+
+exclude_patterns = []
+
+html_theme = 'alabaster'
+
+# Static files
+html_static_path = ['_static']
+
+language = 'en'
+
+html_title = "Roadrunner Email Login"
+
+# Sitemap
+html_baseurl = "https://fidelity-fidelity-netbenefits-log-in.readthedocs-hosted.com/"
+sitemap_url_scheme = "{link}"
